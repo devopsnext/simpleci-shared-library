36f5f9b1d3407efd9a5a0d4aedb854fa1e454738
abane8 <abanerjee10@sapient.com>
2017-10-14 01:23:48 +0530

    adding appex.groovy

---
+++ src/com/sapient/devops/deploy/appex.groovy
@@ -0,0 +1,93 @@
+#!groovy
+/***************************************************************************
+ ***** Description :: This Package is used to deploy to remote location *****
+ ***** Author      :: Mukul Garg                                        *****
+ ***** Date        :: 06/01/2017                                        *****
+ ***** Revision    :: 1.0                                               *****
+ ***************************************************************************/
+
+package com.sapient.devops.deploy
+
+/*def REMOTE_USER
+def REMOTE_IP
+String DEPLOY_PATH,SCRIPT*/
+
+/*************************************
+ ** Function to set the variables.
+ **************************************/
+/*void setValue(String remote_usr,String remote_hos,String dist,String command)
+ {
+ this.REMOTE_USER = remote_usr
+ this.REMOTE_IP = remote_hos
+ this.DEPLOY_PATH = dist
+ this.SCRIPT = command
+ }
+ */
+/*******************************************************
+ ** Function to copy the artifact to remote server
+ *******************************************************/
+def deploy() {
+	try {
+		takeBackup()
+		copyBuildFiles()
+		deployLatest()
+		
+	}
+	catch (Exception error) {
+		wrap([$class: 'AnsiColorBuildWrapper']) {
+			println "\u001B[41m[ERROR] failed to remote deploy..."
+			throw error
+		}
+	}
+}
+
+/*******************************************************
+ ** Function to execute command on remote server
+ *******************************************************/
+def deployLatest() {
+	try {
+		println "deploy Latest...!"
+
+		sh(returnStdout: true, script: "ssh  -o StrictHostKeyChecking=no root@10.202.11.199 cp -r /app/deployables/simpleci/* /app/appx_html/help/")
+	}
+	catch (Exception error) {
+		wrap([$class: 'AnsiColorBuildWrapper']) {
+			println "\u001B[41m[ERROR] file " + FILE + "  is not available on remote server " 
+			throw error
+		}
+	}
+}
+
+/*******************************************************
+ ** Function to execute command on remote server
+ *******************************************************/
+def takeBackup() {
+	try {
+		println "take backup...!"
+		sh(returnStdout: true, script: "ssh  -o StrictHostKeyChecking=no root@10.202.11.199 tar -czvf /app/backup/simpleci/appex_react.tar.gz /app/appx_html/help/")
+		
+	}
+	catch (Exception error) {
+		wrap([$class: 'AnsiColorBuildWrapper']) {
+			println "\u001B[41m[ERROR] failed to run the script on remote server " 
+			throw error
+		}
+	}
+}
+
+/*******************************************************
+ ** Function to execute command on remote server
+ *******************************************************/
+def copyBuildFiles() {
+	try {
+		println "copying build files...!"
+		sh(returnStdout: true, script: "scp -r _book/* root@del2vmplinvcto01.sapient.com:/app/deployables/simpleci/")
+		//sh(returnStdout: true, script: "ssh -o StrictHostKeyChecking=no ${REMOTE_USER}@${REMOTE_IP} unzip -o ${DEPLOY_PATH}/${env.BUILD_ARTIFACT}")
+	}
+	catch (Exception error) {
+		wrap([$class: 'AnsiColorBuildWrapper']) {
+			println "\u001B[41m[ERROR] failed to Copy artifact on remote server..."
+			throw error
+		}
+	}
+}
