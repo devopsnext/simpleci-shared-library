27a7861d180e6008ff259016530cedaa90ee48e3
Abhigyan Banerjee <abanerjee10@sapient.com>
2017-10-17 06:21:38 +0000

    appex.groovy edited online with Bitbucket

--- src/com/sapient/devops/deploy/appex.groovy
+++ src/com/sapient/devops/deploy/appex.groovy
@@ -32,7 +32,7 @@ String HOST_NAME
 def deploy() {
 	try {
       
-      	echo "DEV_HOST  : "+$DEV_HOST
+      	echo "DEV_HOST  : "+HOST_NAME
       	setupHost()
 		copyBuildFiles()
 		deployLatest()
