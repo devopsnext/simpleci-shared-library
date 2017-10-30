c157cb707971f53a5f81affbb07b4286ef3b9994
abane8 <abanerjee10@sapient.com>
2017-10-30 14:42:15 +0530

    appex plugins devops

---
+++ vars/appex_Plugins.groovy
@@ -0,0 +1,33 @@
+#!groovy
+/********************************************************************************
+***** Description :: This Custom Library is used to Deploy to Remote Server *****
+***** Author      :: Mukul Garg                                             *****
+***** Date        :: 04/24/2017                                             *****
+***** Revision    :: 2.0                                                    *****
+********************************************************************************/
+
+import com.sapient.devops.deploy.appex
+
+def call(body) {
+  echo "body : "+body
+  def config = [:]
+  echo "config obj : "+config
+
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  try {
+      def remoteappex = new appex()
+       remoteappex.setValue()
+	   remoteappex.copyPlugins()
+	   remoteappex.takePluginsBackup()
+  }
+  catch (Exception error)
+  {
+      wrap([$class: 'AnsiColorBuildWrapper']) {
+         echo "\u001B[41m[ERROR] ${error}"
+         throw error
+      }
+  }
+}
