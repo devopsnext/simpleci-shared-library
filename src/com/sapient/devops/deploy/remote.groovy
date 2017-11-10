c086aae5b1ca953fedb6cc1cef54cf7b8b9ff23c
Jagadeeswar Chirasani <jchirasani@sapient.com>
2017-11-10 08:43:35 +0000

    remote.groovy edited online with Bitbucket

--- src/com/sapient/devops/deploy/remote.groovy
+++ src/com/sapient/devops/deploy/remote.groovy
@@ -104,7 +104,7 @@ def copyArtifact()
 	 
 	 println "\u001B[32m[INFO] Copying the artifact ${env.WORKSPACE} on server " + REMOTE_IP
 	   
-	 sh(returnStdout: true, script: "scp -o StrictHostKeyChecking=no -r ${REMOTE_USER}/$BUILD_ARTIFACT ${REMOTE_USER}@${REMOTE_IP}:${DEPLOY_PATH}")
+	 sh(returnStdout: true, script: "scp -o StrictHostKeyChecking=no -r ${COPY_PATH}/$BUILD_ARTIFACT ${REMOTE_USER}@${REMOTE_IP}:${DEPLOY_PATH}")
 	 
    }
    catch (Exception error) {
