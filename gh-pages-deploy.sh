caf036fc7ff1cc1bec4b487ffd1de7e13bb65bf4
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-04-19 18:20:46 +0530

    Update gh-pages-deploy.sh

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -11,5 +11,5 @@ git config user.name "Travis CI Publisher"
 git remote add docu "git@github.com:$TRAVIS_REPO_SLUG.git";
 git fetch docu gh-pages:gh-pages
 echo "Pushing to gh-pages of repository $TRAVIS_REPO_SLUG"
-cd /documentation
+cd documentation
 mkdocs gh-deploy -v --clean --remote-name docu
