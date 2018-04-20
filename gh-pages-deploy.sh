0e9ebc174f635cac5c312a7c1eaab74154ac2e19
nbansal16 <36689548+nbansal16@users.noreply.github.com>
2018-04-20 09:42:51 +0530

    Update gh-pages-deploy.sh

--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -2,11 +2,11 @@
 
 echo "Found change on master: Deployment of documentation"
 
-PRIVATE_KEY="cfg/id_rsa"
+#PRIVATE_KEY="cfg/id_rsa"
 
-chmod 600 "${PRIVATE_KEY}"
-eval `ssh-agent -s`
-ssh-add "${PRIVATE_KEY}"
+#chmod 600 "${PRIVATE_KEY}"
+#eval `ssh-agent -s`
+#ssh-add "${PRIVATE_KEY}"
 git config user.name "Travis CI Publisher"
 git remote add docu "git@github.com:$TRAVIS_REPO_SLUG.git";
 git fetch docu gh-pages:gh-pages
